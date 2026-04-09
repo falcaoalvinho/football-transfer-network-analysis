ed6001ace116bdcc94a9f02141c7911aaa536ddf
Álvaro José <josealvaro3421@gmail.com>
2026-04-08 22:06:28 -0300

    fix: wrong condition on if statement; refactor: removed start_nodes and end_nodes variables in order to upgrade redability

--- src/graph.py
+++ src/graph.py
@@ -4,10 +4,15 @@ import requests
 
 load_dotenv()
 
+
+
 def get_api_page(api_url: str, page_name: str):
     """
     Acessa o banco de dados criado via Sheety, e retorna os dados de uma das páginas do arquivo do 
     Google Sheets original
+
+    Obs: Essa função não tem retornos explicítos porque eles podem ser de tipos variados, para mais
+    informações consulte a documentação da biblioteca "request"
 
     api_url   -> Recebe a URL do tipo get para acessar a API 
     page_name -> Recebe o nome da página de onde queremos os dados
@@ -15,12 +20,12 @@ def get_api_page(api_url: str, page_name: str):
     response = requests.get(api_url)
 
     if response.status_code == 200:
-        response = response.json()
-        return response[page_name]
+        response_json = response.json()
+        return response_json[page_name]
 
     
 
-def build_di_graph(page: list, from_str: str, to_str: str) -> nx.DiGraph:
+def build_di_graph(page: list, source_column: str, destiny_column: str) -> nx.DiGraph:
     """
     Recebe uma página da planilha na forma de uma lista de dicionários, itera entre cada um deles e atribuí a eles os devidos plots
     na construção do grafo direcional, considerando inclusive o sentido das relações.
@@ -28,33 +33,32 @@ def build_di_graph(page: list, from_str: str, to_str: str) -> nx.DiGraph:
     page     -> Lista de dicionários contendo os dados para construção do grafo
 
     PARA CONSTRUIR O SENTIDO DOS PLOTS
-    from_str -> Nome do coluna na tabela em que estão dos dados de onde surgem as relações 
-    to_str -> Nome do coluna na tabela em que estão dos dados de para onde seguem as relações 
+    source_column -> Nome do coluna na tabela em que estão dos dados de onde surgem as relações 
+    destiny_column -> Nome do coluna na tabela em que estão dos dados de para onde seguem as relações 
     """
     graph = nx.DiGraph()
 
-    start_nodes = [node[from_str] for node in page]
-    end_nodes = [node[to_str] for node in page]
+    for dict_obj in page:
+        source, destiny = dict_obj[source_column], dict_obj[destiny_column]
 
-    for i in range(len(page)):
-        if not graph.has_edge(from_str, to_str):
+        if not graph.has_edge(source, destiny):
             graph.add_edge(
-                start_nodes[i],
-                end_nodes[i],
+                source,
+                destiny,
                 weight=1,
                 edge_transfers=[],
-                transfers_quantity=0
+                transfers_quantity=1
                 )
             
         else:
             # Quantidade de transferências na Aresta
-            graph[start_nodes[i]][end_nodes[i]]["transfers_quantity"] += 1
-            transfers_quantity = graph[start_nodes[i]][end_nodes[i]]["transfers_quantity"]
+            graph[source][destiny]["transfers_quantity"] += 1
+            transfers_quantity = graph[source][destiny]["transfers_quantity"]
 
             # Transferências na Aresta
-            graph[start_nodes[i]][end_nodes[i]]["edge_transfers"].append(page[i])
+            graph[source][destiny]["edge_transfers"].append(dict_obj)
 
             # Peso associado à Aresta
-            graph[start_nodes[i]][end_nodes[i]]["weight"] = 1 / transfers_quantity
+            graph[source][destiny]["weight"] = 1 / transfers_quantity
         
     return graph
