5f529737ef128f17a1fbcdd7728f329dd8711147
Álvaro José <josealvaro3421@gmail.com>
2026-04-16 15:31:45 -0300

    feat: created visualization methods, draw_di_graph, draw_graph_communities and dict_to_hist

--- src/visualization.py
+++ src/visualization.py
@@ -1,4 +1,7 @@
+import matplotlib.pyplot as plt
+
 COLORS = [
+
 "#FF0000",
 "#FF4D00",
 "#FF9900",
@@ -19,3 +22,12 @@ COLORS = [
 "#FF00E6",
 "#FF0099"
 ]
+
+def dict_to_histogram(data_set: dict, bins: int,) -> None:
+    data_list = [(key, value) for key, value in data_set.items()]
+
+    values, columns, bars = plt.hist(data_list, bins=bins, edgecolor="black")
+    plt.bar_label(bars, fontsize=10, color="black")
+    plt.title("Distribuição das Centralidades dos Vértices")
+    plt.xlabel("Centralidade dos Vértices")
+    plt.show()
